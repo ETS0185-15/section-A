3bf72a73afbdc9ee817885df6a4fb29e5b28c98f
aymen <amuni0322@gmail.com>
2025-03-20 11:58:43 -0700

    day 2 on string

---
+++ test/test02.py
@@ -0,0 +1,7 @@
+text = "thise is to see the TITLE "
+text2 = "not sure what to say"
+text3 = "this Text is for showing the Swap case"
+print (text.title())
+print (text2.capitalize())
+print (text3.swapcase())
+
--- test/upper.py
+++ test/upper.py
@@ -1,6 +1,8 @@
 text = "i am aymen"
+
+text2 = "I AM TRYING TO SHOW"
+
+print (text2.lower())
+
+
 print (text.upper())
-
-
-
-
