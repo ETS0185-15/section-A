a2376b783828d0dfdba86e98e2aaa92e652923a5
aymen <amuni0322@gmail.com>
2025-03-22 13:52:19 -0700

    test 04

---
+++ string types/test04.py
@@ -0,0 +1,20 @@
+text = "this text is created for a purpose of testing strings"
+endswith_1 = text.endswith("strings")
+endswith_2 = text.endswith("bro")
+print(endswith_1)
+print(endswith_2)
+
+
+count = text.count("t")
+count_2 = text.count("z")
+print(count)
+print(count_2)
+
+
+print(text)
+replace = text.replace("testing" , "what")
+print(replace)
+
+print(text.replace("string","wow"))
+
+
