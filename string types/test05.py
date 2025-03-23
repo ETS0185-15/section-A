f9d46e55c897dbdaa9ea96f6bec9c8a934800f10
aymen <amuni0322@gmail.com>
2025-03-23 13:11:05 -0700

    test05

---
+++ string types/test05.py
@@ -0,0 +1,12 @@
+text = "   thise      text is to show or test the strip strings     "
+print(text.strip())
+print(text.lstrip())
+print(text.rstrip())
+
+text_1 = text.strip()
+text_2 = text.lstrip()
+text_3 = text.rstrip()
+
+print(text_1)
+print(text_2)
+print(text_3)
