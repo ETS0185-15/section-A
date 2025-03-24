984c659488fb5d311136bdf452d8896376d0d9e2
aymen <amuni0322@gmail.com>
2025-03-24 10:21:03 -0700

    test06

---
+++ string types/test06.py
@@ -0,0 +1,13 @@
+text = "apple,banana,tomato"
+print(text.split(","))
+text_2 = "today is the 6th day of my commit"
+print(text_2.split(' ', 1))
+
+text_3 = ["today","is","nice"]
+print(' '.join(text_3))
+
+text_4 = "notingcangetbetweenus"
+text_5 = "noting can get between us3"
+print(text_4.isalpha())
+print(text_5.isalpha())
+
