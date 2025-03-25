a43ddaee8f750c9572117a18791d94db3a8e2a3e
aymen <amuni0322@gmail.com>
2025-03-25 11:41:38 -0700

    test07

---
+++ string types/test07.py
@@ -0,0 +1,20 @@
+print("str.isdigit")
+text = "0123"
+text_2 = "01b"
+print(text.isdigit())
+print(text_2.isdigit())
+
+
+print("str.isalnum")
+text_3 = "trying07"
+text_4 = "trying 07"
+print(text_3.isalnum())
+print(text_4.isalnum())
+
+
+print("str.isspace")
+text_5 = " \t\n"
+text_6 = " a "
+print(text_5.isspace())
+print(text_6.isspace())
+
