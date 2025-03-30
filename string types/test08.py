3d78b1f364e95210f383d9a6149382b2dab9eb05
aymen <amuni0322@gmail.com>
2025-03-30 11:13:34 -0700

    test08

---
+++ string types/test08.py
@@ -0,0 +1,17 @@
+
+# to insert mark with name 
+print("{} scored {}".format ("amir",89))
+# to perform with positional and name argumented
+print("{0} scord {1} on{subject}.".format ("emad", 95 ,subject = "bio" ))
+
+
+# f-strings
+text = "thise text is created"
+test_number = 8
+print(f"{text} on test number {test_number}")
+print(f"{text} on test number {test_number + 1}")
+
+
+# len
+print(len(text))
+
