76e3db52776ef5653abdf3f346b5f86dc2c5261a
aymen <amuni0322@gmail.com>
2025-04-02 11:41:19 -0700

    test 03 on list

---
+++ list types/test03.py
@@ -0,0 +1,16 @@
+# insert
+list = ["one","two","three","four"]
+list.insert(2,"ten")
+print(list)
+
+# pop
+
+list2 = ["banana","apple","papaya","watermelon"]
+list2.pop()
+list2.pop(1)
+print(list2)
+
+# remove
+list3 = ["blue","red","green","red","yellow"]
+list3.remove("red")
+print(list3)
