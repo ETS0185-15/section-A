2d55b58bc19b02ed906d66bbdd94e92b69f22a7d
aymen <amuni0322@gmail.com>
2025-04-03 13:28:28 -0700

    test04

---
+++ list types/test05.py
@@ -0,0 +1,16 @@
+# reverse
+list = [1,2,3,4,5,6,7]
+list.reverse()
+print(list)
+
+
+# sort
+number = [1,3,5,1,10,4,7]
+number.sort()
+print(number)
+
+list2 =["ethiopia","america","china"]
+list2.sort(key=len)
+print(list2)
+number.sort(reverse=True)
+print(number)
