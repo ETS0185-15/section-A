5a962d78b16b87d43b9e62e5075f6bb76d183ca3
aymen <amuni0322@gmail.com>
2025-04-11 12:17:03 -0700

    tuples done

---
+++ tuples/README on tuples.md
@@ -0,0 +1,10 @@
+Tuples are ordered sequence of elements like list but we can't be modified after creation.
+- the elements can be accessed using zero besed indexing
+- tuple operattions 
+# count()
+returns the occurrence of a value
+syntax = value.count()
+# index()
+returns the index of a values
+syntax = value.index()
+ 
---
+++ tuples/test01.py
@@ -0,0 +1,25 @@
+# testing how to creat tuples
+tuple01 = ()
+tuple02 = (1,2,"all",2.11)
+tuple03 = 1,"bro",2,8.8
+print(tuple01)
+print(tuple02)
+print(tuple03)
+
+# slicing tuples
+values = (1,2,3,4,5,6,7,8,4)
+print(values[0])
+print(values[3])
+print(values[7])
+
+# using of values 
+print("printing number of occerance")
+print(values.count(4))
+print(values.count(16))
+
+#using of index
+print("using of index")
+print(values.index(1))
+print(values.index(5))
+print(values.index(8))
+
